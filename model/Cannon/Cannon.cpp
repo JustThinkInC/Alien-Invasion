2fba1fd150ba33c07e093297802bd4718930f848
George K <gkh36@uclive.ac.nz>
2019-04-06 16:30:16 +1300

    Smoother animation, position fixes for cannonBall

--- model/Cannon/Cannon.cpp
+++ model/Cannon/Cannon.cpp
@@ -8,6 +8,17 @@ using namespace std;
 
 Cannon* cannons[4];
 
+
+void Cannon::setCannonBall() {
+    float theta = (tilt * M_PI) / 180;
+    cannonBallX = cannonX;
+    cannonBallY = cannonY + (length * 0.2 * cos(theta)) - 2;
+    cannonYOffset = cannonBallY;
+    cannonBallZ = cannonZ + cos(theta) + (0.1 * length) - 2;
+    cannonZOffset = cannonZ + 5;
+}
+
+
 //-- Loads mesh data in OFF format    -------------------------------------
 void Cannon::loadMeshFile(const char* fname)
 {
@@ -49,6 +60,7 @@ void Cannon::loadMeshFile(const char* fname)
 	cout << " File successfully read." << endl;
 }
 
+
 //--Function to compute the normal vector of a triangle with index tindx ----------
 void Cannon::normal(int tindx)
 {
@@ -61,6 +73,7 @@ void Cannon::normal(int tindx)
 	nz = x1*(y2-y3) + x2*(y3-y1) + x3*(y1-y2);
 	glNormal3f(nx, ny, nz);
 }
+
 
 //--------draws the mesh model of the cannon----------------------------
 void Cannon::drawCannon()
@@ -86,8 +99,8 @@ void Cannon::drawCannon()
 
     // Cannon ball
     glPushMatrix();
-        glTranslatef(cannonX, cannonY + 6 + cannonBallY, cannonZ + (length / 8)  + cannonBallX); //glTranslatef(38.88+cx, 64+cy, 0+cz);
-        glColor3f(0, 0, 0);
+        glTranslatef(cannonBallX, cannonBallY, cannonBallZ);
+        glColor3f(1, 0, 0);
         glScalef(0.2, 0.2, 0.2);
         glutSolidSphere(5, 36, 18);
     glPopMatrix();
@@ -96,21 +109,6 @@ void Cannon::drawCannon()
 
 //void display()
 //{
-//	float lpos[4] = {100., 100., 100., 1.0};  //light's position
-//
-//	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);    //GL_LINE = Wireframe;   GL_FILL = Solid
-//	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//	glMatrixMode(GL_MODELVIEW);
-//	glLoadIdentity();
-//	gluLookAt(0, cam_hgt, 200, 0, 0, 0, 0, 1, 0);
-//	glLightfv(GL_LIGHT0, GL_POSITION, lpos);   //set light position
-//
-//    glRotatef(angle, 0.0, 1.0, 0.0);		//rotate the whole scene
-//
-//	drawFloor();
-//
-//    //drawCannon();
-//
 //    //--start here
 //    // First bracket set
 //    glPushMatrix();
@@ -141,140 +139,9 @@ void Cannon::drawCannon()
 //        glColor3f(1, 0.64, 0);
 //        glutSolidCube(1);
 //    glPopMatrix();
-//
-//    // Upwards tilted cannon...classic artillery position
-//    glPushMatrix();
-//      //  glTranslatef(-20, 30, 0);
-//        glRotatef(30, 0, 0, 30);
-//        glScalef(0.8, 0.8, 0.8);
-//        //glTranslatef(20, -30, 0);
-//        //drawCannon();
-//    glPopMatrix();
-//
-//    // Cannon ball
-//    glPushMatrix();
-//        glTranslatef(20+cx, 40+cy, 0);//glTranslatef(38.88+cx, 64+cy, 0+cz);
-//        glColor3f(0, 0, 0);
-//        glutSolidSphere(5, 36, 18);
-//    glPopMatrix();
-//
-//
-//    if (smoke && cy <= -64) { // y position of cannon ball is 0 or less
-//        /*glPushMatrix();
-//         // Value of x when y is 0 in postive region, add offset
-//        // See formula in cannon fire
-//            glTranslatef(280, sy, 0);
-//            glRotatef(90, 90, 0, 0);
-//            glColor3f(0.8, 0.8, 0.8);
-//            glutSolidTorus(4, 10, 100, 100);
-//        glPopMatrix();*/
-//        /**
-//         * formula of circle: r^2 = (x-280)^2 + (y-0)^2 where r=10 here
-//         * the y is z in this case. x is bound from 270 to 290
-//         * So (y)^2 = r^2 - (x-280) ^ 2 -> y = sqrt(r^2 - (x-280)^2)
-//         * Probably should use a timer managing these values.
-//        **/
-//        glPushMatrix();
-//            glTranslatef(270, sy, 0);
-//            glRotatef(90, 90, 0, 0);
-//            glColor3f(0.8, 0.8, 0.8);
-//            glutSolidTorus(1, 2, 100, 100);
-//        glPopMatrix();
-//        glPushMatrix();
-//            glTranslatef(290, sy, 0);
-//            glRotatef(90, 90, 0, 0);
-//            glColor3f(0.8, 0.8, 0.8);
-//            glutSolidTorus(1, 2, 100, 100);
-//        glPopMatrix();
-//        glPushMatrix();
-//            glTranslatef(270, sy, 10);
-//            glRotatef(90, 90, 0, 0);
-//            glColor3f(0.8, 0.8, 0.8);
-//            glutSolidTorus(1, 2, 100, 100);
-//        glPopMatrix();
-//        glPushMatrix();
-//            glTranslatef(290, sy, -10);
-//            glRotatef(90, 90, 0, 0);
-//            glColor3f(0.8, 0.8, 0.8);
-//            glutSolidTorus(1, 2, 100, 100);
-//        glPopMatrix();
-//        glPushMatrix();
-//            glTranslatef(290, sy, 10);
-//            glRotatef(90, 90, 0, 0);
-//            glColor3f(0.8, 0.8, 0.8);
-//            glutSolidTorus(1, 2, 100, 100);
-//        glPopMatrix();
-//        glPushMatrix();
-//            glTranslatef(270, sy, -10);
-//            glRotatef(90, 90, 0, 0);
-//            glColor3f(0.8, 0.8, 0.8);
-//            glutSolidTorus(1, 2, 100, 100);
-//        glPopMatrix();
-//    }
-//    glutSwapBuffers();
-//	//glFlush();
 //}
 
 
-//------- Initialize OpenGL parameters -----------------------------------
-//void initialize()
-//{
-//    //loadMeshFile("../assets/Cannon.off");				//Specify mesh file name here
-//	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);	//Background colour
-////
-////	glEnable(GL_LIGHTING);					//Enable OpenGL states
-////	glEnable(GL_LIGHT0);
-//// 	glEnable(GL_COLOR_MATERIAL);
-////	glEnable(GL_DEPTH_TEST);
-////	glEnable(GL_NORMALIZE);
-////
-////	glMatrixMode(GL_PROJECTION);
-////	glLoadIdentity();
-////	gluPerspective(120, 1, 50, 1000);  //The camera view volume
-//}  //y =ax2 + bx + c
-//y = 50; x = 50
-//x = 38.88, sy = 64
-
-
-//void smokeTimer(int value) {
-//    if (value < 3) {
-//        smoke = true;
-//        sy++;
-//        value++;
-//        glutPostRedisplay();
-//        glutTimerFunc(50, smokeTimer, value);
-//    } else {
-//        sy = 0;
-//        smoke = false;
-//        glutPostRedisplay();
-//    }
-//}
-
-// This version uses a parabola
-/*
-void cannonFire(int value) {
-    firing = true;
-    float a = -0.005f;
-    float b = 1;
-    float c = 4;
-
-    if(value < 50) { //5 seconds
-        cy = a * (cx*cx) + b * cx + c;
-        cx += 10;
-        //cout <<  cx << ", " << cy << endl;
-        value++;
-        glutPostRedisplay();
-        glutTimerFunc(50, cannonFire, value);
-        if (cy <= -64) glutTimerFunc(0, smokeTimer, 0);
-
-    } else {
-        firing = false;
-        cx = 0;
-        cy = 0;
-        cz = 0;
-        glutPostRedisplay();
-    }
-}*/
 
 void fireCannonAnim(int index) {
     Cannon* cannon = cannons[index];
@@ -284,32 +151,24 @@ void fireCannonAnim(int index) {
 
     float g = 9.81;
     float theta = (cannon->tilt * M_PI) / 180;
-    float velocity = 50;
+    float velocity = 30;
     float vertV = velocity * sin(theta) - (g * *value);
     float time = (2 * velocity * sin(theta)) / g;
     double maxHeight = (velocity * time) - (0.5 * g * time * time);
     double range = (velocity * velocity * 2 * sin(2*theta) * cos(theta)) / 9.81;   // 2vInit^2 * sin(theta)*cos(theta) / 9.81
 
-    //y = height - 4.9 * ( x / (v * cos(theta)) * ( x / (v * cos(theta)) + tan(theta)
-    if (time == 0 && *value < 10) {
-        cannon->cannonBallX += velocity / 5 ;
+    if (*value < time * 10) {
+        cannon->cannonBallZ = velocity * *value * cos(theta) + cannon->cannonZOffset;
+        cannon->cannonBallY = cannon->cannonYOffset + (velocity * *value * cos(theta)) - (0.5 * g * *value * *value);
         *value = *value + 1;
+
         glutPostRedisplay();
-        glutTimerFunc(60, fireCannonAnim, index);
-    } else if (*value < time * 10) {
-        cannon->cannonBallX = velocity * *value * cos(theta);//(velocity * cos(theta)) * value - (0.5 * g * value * value);//(38.88 + velocity * value) - (0.5 * g * value * value); value * velocity;
-        cannon->cannonBallY = (velocity * *value * cos(theta)) - (0.5 * g * *value * *value);//tan(theta) * cx - (g / (2 * velocity * velocity * cos(theta) * cos(theta))) * cx * cx;
-        //(velocity * value * cos(theta)) - (0.5 * g * value * value);//0.5 - 4.9 * ( cx / (velocity * cos(theta))) * ( cx / (velocity * cos(theta))) + tan(theta) * cx;
-        //(velocity * time) - (0.5*9.81*time*time); //note the -3 is an offset to look natural
-        *value = *value + 1;
-        glutPostRedisplay();
-        glutTimerFunc(60, fireCannonAnim, index);
-       // if (cy < 40) glutTimerFunc(0, smokeTimer, 0);
+        glutTimerFunc(100, fireCannonAnim, index);
     } else {
         cannon->firing = false;
-        cannon->cannonBallX = 0;
-        cannon->cannonBallY = 0;
+        cannon->setCannonBall();
         *value = 0;
+
         glutPostRedisplay();
     }
 }
--- model/Cannon/Cannon.h
+++ model/Cannon/Cannon.h
@@ -8,14 +8,14 @@
 
 class Cannon {
 private:
-    const int length = 60;
+    const int length = 70;
     float *x, *y, *z;  //vertex coordinate arrays
     int *t1, *t2, *t3; //triangles
     int nvrt, ntri;    //total number of vertices and triangles
 
 public:
     float cannonX, cannonY, cannonZ, tilt = 0;
-    double cannonBallX = 0, cannonBallY = 0, sy = -1;
+    double cannonBallX = 0, cannonBallY = 0, cannonBallZ = 0, sy = -1, cannonYOffset = 0, cannonZOffset = 0;
     bool smoke = false;
     bool firing = false;
 
@@ -29,6 +29,8 @@ public:
     void loadMeshFile(const char* fname);
     void normal(int tindx);
     void drawCannon();
+    void setCannonBall();
+
     int getLength();
 
     Cannon() {}
@@ -43,4 +45,4 @@ void keyboard(unsigned char key, int x, int y);
 void special(int key, int x, int y);
 
 
-#endif //LAB1_CANNON_H
+#endif
